81a4d039e17bf4cf328329179fe318a68196279f
Sameer <sameer@Sameers-MacBook-Pro.local>
2021-06-18 18:09:39 +0530

    TV show api search

---
+++ app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('#searchform')
+form.addEventListener("submit", async function (e) {
+    e.preventDefault()
+    const searchTerm = form.elements.query.value
+    const config={params: {q:searchTerm}}
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`,config )
+    makeimages(res.data)
+    
+})
+
+const makeimages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG')
+            img.src = result.show.image.medium
+            document.body.append(img)
+        }
+    }
+}
